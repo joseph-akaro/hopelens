4b17bede01cc06909d57a6c0d824eab77c26f817
Joseph Akaro <josephmmakaro@gmail.com>
2026-03-13 21:08:02 +0300

    fixed: Link url bug in side-menu.tsx

--- components/side-menu.tsx
+++ components/side-menu.tsx
@@ -14,7 +14,7 @@ export const SideMenu = ({icon, title, routeLink} : sideMenuProps) => {
   return (
     <SidebarGroup className='p-2'>
         <SidebarGroupLabel className='flex gap-1 flex-row justify-center'>
-            <Link href={routeLink} className='flex items-start gap-2 justify-start hover:bg-slate-600 w-full p-2 rounded-md'>
+            <Link href={`${routeLink}`} className='flex items-start gap-2 justify-start hover:bg-slate-600 w-full p-2 rounded-md'>
                 <span className='w-2 h-2  text-sm text-slate-100 mx-4'>{icon}</span>
                 <span className='text-sm font-medium text-slate-100'>{title}</span>
             </Link>
