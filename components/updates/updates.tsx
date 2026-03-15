a04ab4f5e0b43e0703e8996ff21ede96163cc76c
Joseph Akaro <josephmmakaro@gmail.com>
2026-03-15 17:14:07 +0300

    Completed

--- components/updates/updates.tsx
+++ components/updates/updates.tsx
@@ -4,7 +4,7 @@ import { DashHeader } from '../dash-header'
 export const Updates = () => {
   return (
      <div className='flex flex-col w-full h-full flex-1 gap-4'>
-        <DashHeader description='Research Projects in the region' buttonTitle='Add Project' buttonvisibility={true} />
+        <DashHeader description='Research updates submitted by champions' buttonTitle='Add Project' buttonvisibility={true} />
      </div>
   )
 }
