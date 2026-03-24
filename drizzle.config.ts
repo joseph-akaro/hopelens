47ddce52180b8d96b8d358aa5cd7bd07383d36cf
Joseph Akaro <josephmmakaro@gmail.com>
2026-03-24 14:40:10 +0300

    resorted to have dizzel

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: "./db/schema",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!
+  },
+  verbose: true,
+  strict: true,
+  entities: {
+    roles: {
+        provider: 'neon'
+    }
+  }
+});
