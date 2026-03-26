dfdb968eaa7c5418f39323c1a93d199199bfe355
Joseph Akaro <josephmmakaro@gmail.com>
2026-03-26 21:04:53 +0300

    country and users are completed

--- app/api/participatingc-countries/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  return NextResponse.json([
-    { status: "active", responses: 12 },
-    { status: "slow", responses: 5 },
-    { status: "Inactive", responses: 6 },
-  ])
-}
