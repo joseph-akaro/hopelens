8cce6323f91432550f4b4dd430aa4e1141d70290
Joseph Akaro <josephmmakaro@gmail.com>
2026-04-03 11:25:03 +0300

    clean country type error

--- lib/services/country.service.ts
+++ lib/services/country.service.ts
@@ -14,7 +14,6 @@ export async function fetchCountries(): Promise<countryType[]> {
         email: country.email,
         phone: country.phone,
         users: country.champion,
-        lastactivity: country.lastActivity,
     }));
 
     console.log(formattedCountry)
